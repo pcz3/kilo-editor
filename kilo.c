2cbc5642043dd55fc7b790c08886193aa10359df
Paul Czeresko <p.czeresko.3@gmail.com>
2019-03-23 09:57:18 -0400

    Clear screen on exit
    The escape sequences for clear terminal screen and move cursor to 0,0
    are now in the break condition processing and the error handler

--- kilo.c
+++ kilo.c
@@ -14,6 +14,9 @@ struct termios orig_termios;
 
 void die(const char * s)
 {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -71,6 +74,9 @@ void editorProcessKeypress()
     switch(c)
     {
     case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+
         exit(0);
         break;
     default:
