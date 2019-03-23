e416210fea938c195643eb760b8b427bab7f0b7e
Paul Czeresko <p.czeresko.3@gmail.com>
2019-03-23 12:17:37 -0400

    Move includes back to c file
    All includes now reside in kilo.c. kilo.h deleted from working directory

--- kilo.c
+++ kilo.c
@@ -1,6 +1,11 @@
-/*** includes ***/
+/** includes ***/
 
-#include "kilo.h"
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
 /*** defines ***/
 
--- kilo.h
+++
@@ -1,6 +0,0 @@
-#include <ctype.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <termios.h>
-#include <unistd.h>
