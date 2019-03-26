769fcea133ed9bb0fb1c69d7a1ebfac452f329a7
Paul Czeresko <p.czeresko.3@gmail.com>
2019-03-26 18:15:50 -0400

    Revert code to arrow key movement only
    advanced-cursor-control branch will now house those changes. I don't
    feel like writing a bunch of code I can't test right now.

--- kilo.c
+++ kilo.c
@@ -23,8 +23,6 @@ typedef enum editorKey
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
-    PAGE_UP,
-    PAGE_DOWN
 } editorKey_t;
 
 /*** data ***/
@@ -96,34 +94,16 @@ int editorReadKey()
         
         if (seq[0] == '[')
         {
-            if (seq[1] >= '0' && seq[1] <= '9')
+            switch (seq[1])
             {
-                if (read(STDOUT_FILENO, &seq[2], 1) != 1)
-                    return '\x1b';
-                if (seq[2] == '~')
-                {
-                    switch (seq[1])
-                    {
-                    case '5':
-                        return PAGE_UP;
-                    case '6':
-                        return PAGE_DOWN;
-                    }
-                }
-            }
-            else
-            {
-                switch (seq[1])
-                {
-                case 'A':
-                    return ARROW_UP;
-                case 'B':
-                    return ARROW_DOWN;
-                case 'C':
-                    return ARROW_RIGHT;
-                case 'D':
-                    return ARROW_LEFT;
-                }
+            case 'A':
+                return ARROW_UP;
+            case 'B':
+                return ARROW_DOWN;
+            case 'C':
+                return ARROW_RIGHT;
+            case 'D':
+                return ARROW_LEFT;
             }
         }
 
@@ -309,16 +289,6 @@ void editorProcessKeypress()
     case ARROW_UP:
     case ARROW_RIGHT:
         editorMoveCursor(c);
-        break;
-    case PAGE_UP:
-    case PAGE_DOWN:
-        {
-        int times = E.screenrows;
-        while (times--)
-            editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
-        }
-        break;
-    default:
         break;
     }
 }
